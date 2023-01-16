fa385cf5ac5a718370129c313425f9968b911feb
faellm <rafaellara1405@gmail.com>
2023-01-16 10:46:25 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("Hello World");
+
+let cor = "verde";
+
+if (cor === 'verde'){
+    console.log("Siga")
+} 
+else if(cor === 'amarelo'){
+    console.log('Atenção')
+} 
+else {
+    console.log("Pare")
+}
