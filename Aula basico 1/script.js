4c194a6dd8294333f0dabc1e1e8e1b1f1dc8141b
faellm <rafaellara1405@gmail.com>
2023-01-16 11:34:30 -0300

    'ajuste de pastas'

---
+++ Aula basico 1/script.js
@@ -0,0 +1,49 @@
+console.log("Hello World");
+
+let cor = "azul";
+/* 
+
+ESTRUTURA COM IF, ELSE IF , ELSE
+
+if (cor === 'verde'){
+    console.log("Siga")
+} 
+else if(cor === 'amarelo'){
+    console.log('Atenção')
+} 
+else {
+    console.log("Pare")
+}
+*/
+
+/* 
+//ESTRUTURA COM switch
+
+switch (cor) {
+
+    case "verde":
+        console.log("Siga");
+        break
+    case "amarelo":
+        console.log('Atenção');
+        break
+    case "vermelho":
+        console.log("Pare");
+        break
+
+
+    default:
+        console.log("Não sei");
+}
+*/
+
+// Estrutura de repetição
+
+// TABUADA
+// template String
+let n = 7;
+
+for (let i = 0 ; i <= 10 ; i++) {
+
+    console.log(`${i} x ${n} = ${i*n}`);
+}
