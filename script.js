285fb0eabd28dde33cb4aa5d387245fbfa740d2d
faellm <rafaellara1405@gmail.com>
2023-01-16 11:32:02 -0300

    'Aula base de javascript'

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 console.log("Hello World");
 
-let cor = "verde";
+let cor = "azul";
+/* 
+
+ESTRUTURA COM IF, ELSE IF , ELSE
 
 if (cor === 'verde'){
     console.log("Siga")
@@ -10,4 +13,37 @@ else if(cor === 'amarelo'){
 } 
 else {
     console.log("Pare")
+}
+*/
+
+/* 
+//ESTRUTURA COM switch
+
+switch (cor) {
+
+    case "verde":
+        console.log("Siga");
+        break
+    case "amarelo":
+        console.log('Atenção');
+        break
+    case "vermelho":
+        console.log("Pare");
+        break
+
+
+    default:
+        console.log("Não sei");
+}
+*/
+
+// Estrutura de repetição
+
+// TABUADA
+// template String
+let n = 7;
+
+for (let i = 0 ; i <= 10 ; i++) {
+
+    console.log(`${i} x ${n} = ${i*n}`);
 }
